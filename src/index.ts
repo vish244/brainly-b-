37fe9a000190076c960502408f7c0d244ec0ddc8
vish244 <v87078523@gmail.com>
2025-01-28 22:30:38 +0530

    mongo

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 import express from "express";
 import {UserModel } from "./db";
+import mongoose from "mongoose";
 
 
 
-
-// async function connection() {
+async function connection() {
 
 //     // await mongoose.connect("mongodb+srv://brainly:HnIBfp3XpCghCCOl@cluster0.8onuc.mongodb.net/");
 //     // await mongoose.connect("mongodb://0.0.0.0:27017");
 
-//     try{
-//         // await mongoose.connect("mongodb+srv://v87078523:DPwVjZCiS1jQicbz@projectnew.tnlv3.mongodb.net/");
-//         // //GedQaTOsQkcSay
-//     }catch(err){
-//         console.log("Error in connection");
-//     }
-// }
-// connection();
+    try{
+        await mongoose.connect("mongodb+srv://v87078523:DPwVjZCiS1jQicbz@projectnew.tnlv3.mongodb.net/");
+        // //GedQaTOsQkcSay
+    }catch(err){
+        console.log("Error in connection");
+    }
+}
+connection();
 
 const app = express();
 app.use(express.json());
